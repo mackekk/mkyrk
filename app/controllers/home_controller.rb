f1dc0a5fc510d3c31bf58f477c72c9f5cdbc053c
Marcus Kyrk <marcus.kyrk2@gmail.com>
2025-04-24 16:36:01 +0200

    feat: Add Om mig and Projekt pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,16 @@ class HomeController < ApplicationController
   def index
     # This is our Hello World page
   end
+
+  def contact
+    # This renders the contact page (app/views/home/contact.html.erb)
+  end
+
+  def about
+    # This renders the about page (app/views/home/about.html.erb)
+  end
+
+  def projects
+    # This renders the projects page (app/views/home/projects.html.erb)
+  end
 end 
